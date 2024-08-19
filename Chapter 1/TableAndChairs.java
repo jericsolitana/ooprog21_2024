598540c13150ad6276a20b7091be9b7ec5eed740
jericsolitana <solitanajericnino@gmail.com>
2024-08-19 16:44:08 +0800

    Update TableAndChairs.java

--- Chapter 1/TableAndChairs.java
+++ Chapter 1/TableAndChairs.java
@@ -3,6 +3,7 @@ public class TableAndChairs
 public static void main(String[] args) 
 {
         System.out.println("X                      X");
+        System.out.println("X                      X");
         System.out.println("X      XXXXXXXXXX      X");
         System.out.println("XXXXX  X        X  XXXXX");
         System.out.println("X  X   X        X  X   X");
