d929465c2ea744711c0f0e968844958232a0d6fb
jericsolitana <solitanajericnino@gmail.com>
2024-08-19 16:39:44 +0800

    Create MovieQuoteInfo.java

---
+++ Chapter 1/MovieQuoteInfo.java
@@ -0,0 +1,11 @@
+public class MovieQuoteInfo
+{
+  public static void main(String[] args){
+
+System.out.println("Qoute: Life isn't some cartoon musical where you sing a little song and all your insipid dreams magically come true. So let it go. ”);
+System.out.println("Movie: Zootopia ");
+System.out.println("Actor: Chief Bogo ");
+System.out.println("Year: 2016 ");
+
+}
+}
