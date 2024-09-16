2c565a538b5c34e9018b885ee4c4699ca85f8c26
jericsolitana <solitanajericnino@gmail.com>
2024-09-16 20:41:53 +0800

    Add files via upload

---
+++ Chapter 2/FirstAddressExercise.java
@@ -0,0 +1,14 @@
+public class FirstAddressExercise{
+
+public static void main(String[] args) {
+  displayAddress();
+}
+public static void displayAddress() {
+    String address = "8900 U.S. Hwy 14";
+    String cityAndZipCode = "Crystal Lake, IL 60014";
+    String company = "Apple Inc.";
+    
+    System.out.print("XYZ Company\n"+address+"\n"+cityAndZipCode+"\n"+company);
+  }
+   
+}
